7864c0c82e7d121852001b63bd3663bff18f464d
Edwige Gros <edwige.gros@u-psud.fr>
2020-07-01 14:16:50 +0200

    copyright

--- delpy/__init__.py
+++ delpy/__init__.py
@@ -1,5 +1,9 @@
 from .delpy import *
-
+"""
+* Copyright (C) 200?-200? DelpyWidget
+* based on code by gwangyi CopyrightHolder copyright (C) 200?-200? Delpy
+* License: gnu.org/licenses/gpl.html GPL version 2 or higher
+"""
 
 def _jupyter_nbextension_paths():
     return [dict(
